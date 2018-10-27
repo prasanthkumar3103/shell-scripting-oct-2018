0dbcb09ff7263b16e25331c4138be163ca97be28
prasanthkumar3103 <36511679+prasanthkumar3103@users.noreply.github.com>
2018-10-27 12:44:15 +0530

    Update 12-if-check-either-dir-or-file-exist.sh

--- 12-if-check-either-dir-or-file-exist.sh
+++ 12-if-check-either-dir-or-file-exist.sh
@@ -22,3 +22,23 @@ else #item doesnt exist
     echo "noting to update...!"
 fi
 
+
+#!/bin/bash
+
+dir_loc=/opt/prashanth
+file_loc=$dir_loc/one.txt
+
+
+if [ -d $dir_loc ]
+then
+    echo "$dir_loc is directory"
+
+    if [ -f $file_loc ]
+    then
+        echo "$file_loc This is file"
+    else
+        "$file_loc file doesnt exist., Please create it manually and Re-Run the script"
+    fi
+else
+    "$dir_loc directory doesnt exist"
+fi
