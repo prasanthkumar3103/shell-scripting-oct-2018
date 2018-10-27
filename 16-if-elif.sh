9e22120f813cd386d28ea6e6b65b2aae4b171cde
prasanthkumar3103 <36511679+prasanthkumar3103@users.noreply.github.com>
2018-10-27 13:35:36 +0530

    Update 16-if-elif.sh

--- 16-if-elif.sh
+++ 16-if-elif.sh
@@ -30,4 +30,21 @@ then
 else
     echo "All of the specified args are different $arg1 $arg2 $arg3"
 fi
-~
+
+#!/bin/sh
+
+a=10
+b=20
+
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
