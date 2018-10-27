a8c9f8ae3560f1c2e859ab7558382f0de5fdb01d
prasanthkumar3103 <36511679+prasanthkumar3103@users.noreply.github.com>
2018-10-27 13:10:39 +0530

    Update 14-if-test-file-is-empty.sh

--- 14-if-test-file-is-empty.sh
+++ 14-if-test-file-is-empty.sh
@@ -16,3 +16,23 @@ then
 else
     echo "File,$file doesnt exist in system"
 fi
+
+
+#!/bin/bash
+
+file_path=/opt/prashanth/one.txt
+
+if [ -f $file_path ]
+then
+    echo "$file_path file is exist and checking for empty or non-empty file"
+
+    if [ -s $file_path ]
+    then
+        echo "file is not empty it will display data else no"
+        echo "displaying file data `cat $file_path`"
+    else
+        echo " file $file_path doesnt exist in fs or file is not having data"
+    fi
+else
+    echo "$file_path invalid directory"
+fi
