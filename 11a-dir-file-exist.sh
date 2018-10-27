5a91a742006d2f4b0461d7b05ef2cfdfc4805b5b
prasanthkumar3103 <36511679+prasanthkumar3103@users.noreply.github.com>
2018-10-27 11:39:02 +0530

    Create 11a-dir-file-exist.sh

---
+++ 11a-dir-file-exist.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+dir_loc=$1
+filename=$2
+
+if [ -e $dir_loc ]
+then
+    echo " $dir_loc directory exist and moving to next"
+     if [ ! -e $dir_loc ]
+     then
+         mkdir $dir_loc
+
+    elif [ ! -e $dir_loc/$filename ]
+    then
+         touch $dir_loc/$filename
+         date +%Y%m%d >>$dir_loc/$filename
+    elif  [ -e $dir_loc/$filename ]
+     then
+         echo "$filename file exist in $dir_loc directory and appending some data to file"
+         date +%Y%m%d >>$dir_loc/$filename
+     fi
+
+fi
